440bb1daf67c2f23a6aa2843144319f1af059738
Writuparna <writu.career@gmail.com>
2025-12-03 12:28:49 +0530

    api modification

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
+import { GET } from "../api/about/route";
+
 export default async function About() {
-  const res = await fetch("http://localhost:3000/api/about");
+  const res = await GET();
   const data = await res.json();
   return (
     <div className="container">    
